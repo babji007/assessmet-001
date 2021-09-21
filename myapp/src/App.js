560d5a180428f5ed19d944278f7e552662332426
Babji <babji.singireddy@mphasis.com>
2021-09-21 17:17:59 +0530

    first changes

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,7 +5,14 @@ import { BrowserRouter as Router, HashRouter,Switch, Route, Link } from 'react-r
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
+// import Dropdown from './components/Dropdown';
+// import Dd from './components/Dd';
+// import Sys from './components/Sys';
+// // import Store from './Store/Store';
+// import ListProduct from './components/ListProduct';
+// import content from '../src/mock/mockcontent';
+// import FavouriteTeam from './components/FavoriteTeam';
 
 
 // import AddProduct from './components/AddProduct';
@@ -13,13 +20,17 @@ import { Provider } from 'react-redux';
 function App() {
   return (
 
-    
+    // <Provider store={Store}>
     <Router>
-    <div>
+    <div class="App">
     <Header/>
-
+    {/* <FavouriteTeam/> */}
+   {/* <Sys/> */}
+    {/* <Dd  /> */}
     <List/>
+    {/* <ListProduct/> */}
     
+    {/* <Dropdown/> */}
 
     <Switch>
           {/* <Route exact path='/' component={Home} /> */}
@@ -31,7 +42,7 @@ function App() {
     <Footer/>
     </div>
     </Router>
-    
+    // </Provider>
   );
 }
 
